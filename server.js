a32e9d1a56a790e351603004b9871f241709d543
navinkumarc <navinkumar.be@gmail.com>
2018-04-09 02:39:12 +0530

    updated

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ MongoClient.connect('mongodb://navin:admin123@ds131989.mlab.com:31989/portal', (
 
 // Reuse database object in request handlers
 app.get("/posts", function(req, res, next) {
-  db.collection("posts").find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+  db.collection("posts").find({ isApproved: { $exists: false } } ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
      if (e) return next(e)
      res.send(results)
    })
@@ -30,9 +30,18 @@ app.post('/posts', function(req, res, next) {
     res.send(results)
   })
 });
+app.put('/approve_posts', function(req, res, next) {
+    console.log(req.body._id)
+  //db.collection("posts").update(req.body, {}, function(e, results){
+  db.collection("posts").update({ '_id': ObjectID(req.body._id) }, {$set: {isApproved:"Y"}}, function(e, results){
+    if (e) return next(e)
+      //console.log(results)
+    res.send(results)
+  })
+});
 
 app.get("/hand-picked-posts", function(req, res, next) {
-  db.collection("posts").find({ IsApproved: { $exists: true } } ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+  db.collection("posts").find({ isApproved: { $exists: true } } ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
      if (e) return next(e)
      res.send(results)
    })
